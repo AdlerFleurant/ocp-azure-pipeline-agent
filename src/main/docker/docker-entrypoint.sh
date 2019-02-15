c5ef2e26f5612f5bfd9593f2f8cc9544b5390296
Adler Fleurant <Adler.Fleurant@picochange.com>
2019-02-15 11:18:57 -0500

    initial rhel azure-devops-agent containerized.

---
+++ src/main/docker/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "[ENTRYPOINT] Azure DevOps Agent on RHEL 7.6-151]"
+
+if [ -z "$SERVER_URL" -a -z "$ACCESS_TOKEN" -a -z "$AGENT_POOL" -a -z "$AGENT_NAME" ]; then
+        echo "[ENTRYPOINT] SERVER_URL, ACCESS_TOKEN, AGENT_POOL, AGENT_NAME have to be populated with correct values."
+        exit 1
+fi
+
+./config.sh --unattended --url $SERVER_URL --auth pat --token $ACCESS_TOKEN --pool $AGENT_POOL --agent $ACCESS_TOKEN --acceptTeeEula
+./run.sh
