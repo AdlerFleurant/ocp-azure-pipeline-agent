4aed5e280fe061a6e615587d3dcbd2e5539ee52e
Adler Fleurant <Adler.Fleurant@picochange.com>
2019-03-20 01:00:43 -0400

    Change Openshift Client to correct version

--- openshift-client/src/main/docker/docker-entrypoint.sh
+++ openshift-client/src/main/docker/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "[ENTRYPOINT] Azure DevOps Agent on RHEL 7.6-151 for openshift client]"
+echo "[ENTRYPOINT] Azure DevOps Agent on RHEL 7.6 for openshift client]"
 
 if [ -z "$SERVER_URL" -a -z "$ACCESS_TOKEN" -a -z "$AGENT_POOL" -a -z "$AGENT_NAME" ]; then
         echo "[ENTRYPOINT] SERVER_URL, ACCESS_TOKEN, AGENT_POOL, AGENT_NAME have to be populated with correct values."
@@ -7,7 +7,7 @@ if [ -z "$SERVER_URL" -a -z "$ACCESS_TOKEN" -a -z "$AGENT_POOL" -a -z "$AGENT_NA
 fi
 
 export GIT_VERSION=2.18
-export OPENSHIFT_CLIENT_VERSION=v3.11.69
+export OPENSHIFT_CLIENT_VERSION=v3.11.88
 
 if ! ./config.sh --unattended --url $SERVER_URL --auth pat --token $ACCESS_TOKEN --pool $AGENT_POOL --agent $AGENT_NAME --acceptTeeEula ; then
     ./config.sh --unattended --url $SERVER_URL --auth pat --token $ACCESS_TOKEN --pool $AGENT_POOL --agent $AGENT_NAME --replace --acceptTeeEula
